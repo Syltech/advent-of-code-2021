f97ce1ac48defbde2ee9c45a31e0721fc7ad37d3
Sylvain Téchené <sylvain.techene@wanadoo.fr>
2021-12-03 16:58:51 +0100

    day 02

---
+++ 02/index.ts
@@ -0,0 +1,43 @@
+import { parseInput } from '../utils';
+
+let input = parseInput('./02/input.txt');
+
+function moveSubmarine(input: string[]): { x: number, y: number } {
+    let coordinates = { x: 0, y: 0 }
+    for (let move of input) {
+        if (move.length === 0) break;
+        const [direction, value] = move.split(' ');
+        if (direction === 'forward') {
+            coordinates.x += +value;
+        } else {
+            coordinates.y += (direction === 'down') ? +value : -value;
+        }
+    }
+    return coordinates;
+}
+
+let result = moveSubmarine(input);
+
+console.log("PART 1");
+console.log({ result, answer: result.x * result.y })
+
+
+function moveSubmarine2(input: string[]): { x: number, y: number, aim: number } {
+    let coordinates = { x: 0, y: 0, aim: 0 }
+    for (let move of input) {
+        if (move.length === 0) break;
+        const [direction, value] = move.split(' ');
+        if (direction === 'forward') {
+            coordinates.x += +value;
+            coordinates.y += +value * coordinates.aim;
+        } else {
+            coordinates.aim += (direction === 'down') ? +value : -value;
+        }
+    }
+    return coordinates;
+}
+
+let result2 = moveSubmarine2(input);
+
+console.log("PART 2");
+console.log({ result2, answer: result2.x * result2.y })
